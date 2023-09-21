9a0843f7b5fb1a54ec5151da08e019d66e5510e5
Amaka Eguzoro <eguzoroa@gmail.com>
2023-09-22 00:12:28 +0100

    firs set up of a basic express server

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  console.log("[TEST]!");
+
+  res.send("Hello from Homepage.");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server is running on port: http://localhost:${PORT}`)
+);
