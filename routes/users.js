c762f9ecad0e0c73987133acfe2c8caf2212be89
Amaka Eguzoro <eguzoroa@gmail.com>
2023-09-23 00:47:44 +0100

    created a local storage for users

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,22 @@ import express from "express";
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 25,
+  },
+];
+
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
+  console.log(users);
   res.send("Hello");
 });
 
