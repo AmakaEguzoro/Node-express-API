57e84c1df405b5ee8893c9060ffbc45bffedf5d6
Amaka Eguzoro <eguzoroa@gmail.com>
2023-09-29 01:06:39 +0100

    learnt how to work with ID for end points

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 20,
-  },
-];
+const users = [];
 
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
@@ -23,10 +13,16 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   const user = req.body;
 
-  users.push(user);
-  res.send(`User with the name ${user.firstName} added to the database!!`);
+  users.push({ ...user, id: uuidv4() });
 
-  get();
+  res.send(`User with the name ${user.firstName} added to the database!!`);
+});
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
 });
 
 export default router;
